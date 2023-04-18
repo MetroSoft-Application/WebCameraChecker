15101012d3a9ac6245dc32c113e61f3d7df940ed
alexisonwire <47860770+alexisonwire@users.noreply.github.com>
2023-04-19 05:03:40 +0900

    first commit

---
+++ StopWatch.js
@@ -0,0 +1,57 @@
+var start;
+var state = 'STOP';
+var timer_id;
+
+// ゼロを追加する
+var AddZero = function (value) {
+    if (value < 10) {
+        value = '0' + value;
+    }
+    return value;
+};
+
+function TimeClear() {
+    document.getElementById('time').innerHTML = '00' + ':' + '00' + ':' + '00';
+}
+
+var GoTimer = function () {
+    var now = new Date();
+
+    var milli = now.getTime() - start.getTime(); // 差をミリ秒で
+    var seconds = Math.floor(milli / 1000); // 秒を取得
+    var minutes = Math.floor(seconds / 60); // 分を取得
+    var hours = Math.floor(minutes / 60); // 時を取得
+
+    milli = ('000' + milli).slice(-3);//ミリ秒の表示桁数指定
+
+    seconds = seconds - minutes * 60;
+    minutes = minutes - hours * 60;
+
+    // 1 桁の場合は 0 を補完
+    hours = AddZero(hours);
+    seconds = AddZero(seconds);
+    minutes = AddZero(minutes);
+
+    //document.getElementById('time').innerHTML = hours + ':' + minutes + ':' + seconds + "." + milli;
+    document.getElementById('time').innerHTML = hours + ':' + minutes + ':' + seconds;
+}
+
+function Start() {
+    start = new Date();
+    timer_id = setInterval(GoTimer, 10);
+}
+
+function Stop() {
+    clearInterval(timer_id);
+}
+
+function StartOrStopControl() {
+    if (state == 'STOP') {
+        start = new Date();
+        timer_id = setInterval(goTimer, 10);
+        state = 'RUNNING';
+    } else {
+        clearInterval(timer_id);
+        state = 'STOP';
+    }
+}
---
+++ VideoManager.js
@@ -0,0 +1,78 @@
+class VideoManager {
+    video = null;
+    nowAngle = 0;
+    stm = null;
+    recorder = null;
+    recordedChunks = [];
+    constructor() {
+    }
+
+    async GetVideo(isAudioEnable) {
+        try {
+            this.video = document.getElementById("video");
+            this.stm = await navigator.mediaDevices.getUserMedia({
+                audio: isAudioEnable,
+                video: true
+            });
+            this.video.srcObject = this.stm;
+            this.video.play();
+            return true;
+        } catch (e) {
+            console.log(e);
+            return false;
+        }
+    }
+
+    StopVideo() {
+        if (this.recorder != null) {
+            this.recorder.stop();
+            return true;
+        }
+        return false;
+    }
+
+    Rotate(cmdAngle) {
+        if (this.video == null) { return; }
+        this.nowAngle = this.nowAngle + cmdAngle;
+        this.video.style.transform = "rotate(" + this.nowAngle + "deg)";
+    }
+
+    Download() {
+        let blob = new Blob(this.recordedChunks, {
+            type: "video/webm"
+        });
+        let url = URL.createObjectURL(blob);
+        let anchor = document.getElementById('Downloadlink');
+        anchor.href = url;
+        anchor.download = this.GetNowYMDhmsStr() + ".webm";
+    }
+
+    Recoading() {
+        if (this.stm == null) { return; }
+        this.recorder = new MediaRecorder(this.stm, {
+            audioBitsPerSecond: 16 * 1000
+        });
+        this.recordedChunks = [];
+        this.recorder.addEventListener('dataavailable', function (event) {
+            if (event.data.size > 0) {
+                this.recordedChunks.push(event.data);
+            }
+        }.bind(this));
+        this.recorder.start();
+        this.recorder.addEventListener('stop', function (event) {
+            this.Download();
+        }.bind(this));
+    }
+
+    GetNowYMDhmsStr() {
+        const date = new Date()
+        const Y = date.getFullYear()
+        const M = ("00" + (date.getMonth() + 1)).slice(-2)
+        const D = ("00" + date.getDate()).slice(-2)
+        const h = ("00" + date.getHours()).slice(-2)
+        const m = ("00" + date.getMinutes()).slice(-2)
+        const s = ("00" + date.getSeconds()).slice(-2)
+
+        return Y + M + D + h + m + s
+    }
+}
